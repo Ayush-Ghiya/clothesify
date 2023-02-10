c152cf602bf127c13d5b317590ed2f676b05fb40
Ayush-Ghiya <ayushghiya2002@gmail.com>
2023-02-10 10:55:44 +0530

    error fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  target: "serverless",
 }
 
+module.exports = {
+  target: "serverless",
+};
 
 module.exports = nextConfig
